dfff71ba7fe2959a2eecad276288493549488f72
gkarumbi <gkarumbi@gmail.com>
2020-07-20 15:42:53 +0300

    Add login route to our views.py file

---
+++ app/auth/views.py
@@ -0,0 +1,18 @@
+from flask import render_template,redirect,url_for,flash,request,abort
+from flask_login import login_user,logout_user, login_required
+from ..models import User 
+from .forms import RegistrationForm,LoginForm
+from .. import  db
+from . import auth
+
+@auth.route('/login', methods = ["GET","POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email= login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    title = 'Watchlist Login'
+    return render_template('auth/login.html', login_form=login_form, title=title)
