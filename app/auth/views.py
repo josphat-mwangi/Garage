1e9e7fc1e2e06cafd087184f8a3a88c8ddb41500
gkarumbi <gkarumbi@gmail.com>
2020-07-20 16:41:10 +0300

    Add logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,3 +28,9 @@ def register():
         title = "New Account"
     return render_template('auth/register.html', registration_form=form)
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
