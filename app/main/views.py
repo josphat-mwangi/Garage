ef3a68a9671c0cbdb81b92d0a7998e954a07973d
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-21 17:36:31 +0300

    create user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
 from flask_login import login_required, current_user
@@ -14,3 +14,11 @@ def index():
     welcome_message = 'Welcome to the Home!'
 
     return render_template('index.html', welcome_message = welcome_message )
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = user.query.filter_by(username = uname).first()
+
+    if user in None:
+        abort(404)
+    return render_template("profile/profile.html", user = user)
